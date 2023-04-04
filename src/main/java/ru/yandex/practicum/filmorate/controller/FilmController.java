ac3febf2ed542f85726ca53e0ec265e246e832cf
Zaven <kalanderyan99@yandex.ru>
2023-04-04 21:23:56 +0300

    feat:реализованы классы контроллеры и модели

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,64 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class FilmController {
+    private static final Logger log = LoggerFactory.getLogger(FilmController.class);
+
+    Map<Integer, Film> films = new HashMap();
+    private int id = 0;
+
+
+    @GetMapping("/films")
+    public Map findAll() {
+        return films;
+    }
+
+    @PostMapping("/film")
+    public Film createFilm(@RequestBody Film film) {
+        film.setId(generateId());
+        if (validate(film)) {
+            films.put(film.getId(), film);
+            log.info("Новый пользователь успешно зарегестрирован {}", film);
+        }
+        return film;
+    }
+
+    @PutMapping("/film")
+    public Film updateFilm(@RequestBody Film film) {
+        if (validate(film)) {
+            films.put(film.getId(), film);
+            log.info("Данные пользователя {} успешно обнавлены.", film);
+        }
+        return film;
+    }
+
+    private int generateId() {
+        return ++id;
+    }
+
+    private boolean validate(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ValidationException("Название фильма не может быть пустым");
+        } else if (film.getDescription().length() > 200) {
+            throw new ValidationException("Описание фильма не может быть больше 200 символов");
+        } else if (film.getReleaseDate().isBefore(LocalDate.of(1895,12,28))) {
+            throw new ValidationException("Дата релиза не может быть раньше чем 28.12.1895");
+        } else if (!film.getDuration().isNegative()) {
+            throw new ValidationException("Продолжительность фильма должна быть положительной.");
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,62 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    Map<Integer, User> users = new HashMap();
+    private int id = 0;
+
+
+    @GetMapping("/users")
+    public Map findAll() {
+        return users;
+    }
+
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user) {
+        user.setId(generateId());
+        if (validate(user)){
+            users.put(user.getId(), user);
+            log.info("Новый пользователь успешно зарегестрирован {}", user);
+        }
+        return user;
+    }
+
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User user) {
+        if (validate(user)){
+            users.put(user.getId(), user);
+            log.info("Данные пользователя {} успешно обнавлены.", user);
+        }
+        return user;
+    }
+
+    private int generateId() {
+        return ++id;
+    }
+
+    private boolean validate(User user){
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            throw new ValidationException("Почта не может быть пустой и должна содержать @");
+        } else if (user.getLogin() == null || user.getLogin().isBlank()) {
+            throw new ValidationException("Логин пустой или содержит в себе пробелы");
+        } else if (user.getName() == null || user.getEmail().isBlank()) {
+            user.setName(user.getLogin());
+        } else if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем.");
+        }
+        return true;
+    }
+
+}
